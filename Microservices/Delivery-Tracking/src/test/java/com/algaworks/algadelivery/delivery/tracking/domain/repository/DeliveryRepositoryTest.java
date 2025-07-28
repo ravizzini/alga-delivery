d93b4381bfccb1530eacc5a5185dd0aaa4751226
victor.ravizzini <victor.ravizzini@gmail.com>
2025-07-28 14:52:09 -0300

    Domain Events, Event-Driven Architecture e Kafka

--- Microservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/repository/DeliveryRepositoryTest.java
+++ Microservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/repository/DeliveryRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.web.service.annotation.PostExchange;
 
 import java.math.BigDecimal;
 import java.time.Duration;
